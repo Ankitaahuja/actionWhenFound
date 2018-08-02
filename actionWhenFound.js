a0343a0c7afbc7712e38b26409fc7a79da2f585c
Ankita Ahuja <ankitaahuja111@gmail.com>
2018-08-02 17:47:31 +0000

    Print the index of Waldo using for each

--- actionWhenFound.js
+++ actionWhenFound.js
@@ -1,17 +1,21 @@
+
 // The second argument/parameter is expected to be a function
 
 function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
-        found(i);   // execute callback
+  var index = 0;
+  // for (var i = 0; i < arr.length; i++)
+    arr.forEach(function(element) {
+      
+      if (element === "Waldo") {
+        found(index);   // execute callback
       }
-    }
+      index++
+    });
   }
   
-  function actionWhenFound(num, n) {
-    console.log("Found Waldo at index", num , "!");
+  function actionWhenFound(index) {
+    console.log("Found Waldo at index", index + "!");
   }
   
   findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-  
-  // "Found Waldo at index 2!".
+//   "Found Waldo at index 2!".
